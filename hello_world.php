03ae7918a78a8a26d488e52354a6b118b6cc2bca
fluczak <fluczak@IT-LUCZAK11.Libri-HEF12.local>
2024-03-29 14:04:46 +0100

    change label

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,6 @@
 <?php
 // test
 echo "<p align=\"center\">Hallo \"Welt\" ich bin da!</p>";
-//echo "ich bin da!</p>";
+//echo "ich bin wieder da!</p>";
 echo "<p align=\"center\">[\"PHP-Kurs.com\"]</p>";
 ?>
